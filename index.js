49cc57ef058f99b0cd10a6ee5af3993f3021b0d1
unknown <muktinathrajbanshi786@gmail.com>
2026-02-02 20:19:58 +0545

    wow it's totally awesome muktinath

--- index.js
+++ index.js
@@ -2,12 +2,23 @@
  const pro2 = document.getElementById("pro2");
 
  const iphone = "images/iphone.png";
- const apple_s_5 = "images/apple_s_5.png";
+ const iphone_blue = "images/iphone_pro.png";
+ const bWatch = "images/apple_s_5.png";
+ const wWatch = "images/apple_s_5_white.png";
+
 
  pro.addEventListener("click", function()  {
     document.getElementById("theImage").src=iphone;
  })
 
   pro2.addEventListener("click", function()  {
-    document.getElementById("theImage").src=apple_s_5;
+    document.getElementById("theImage").src=iphone_blue;
+ })
+
+ $("#black_watch").click(() => {
+        document.getElementById("theWatch").src=bWatch;
+ })
+
+ $("#white_watch").click(() => {
+        document.getElementById("theWatch").src=wWatch;
  })
